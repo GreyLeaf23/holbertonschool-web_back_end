1adffe8c41b167ebda33b11f0e324461678ec3a0
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-03 17:47:33 -0400

    Module defines multiple variables with different data types.

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module defines type-annotated variables """
+
+
+a = 1  # variable a assigned int value 1
+pi = 3.14  # variable pi assigned float value 3.14
+i_understand_annotations = True  # variable i_understand_annotations assigned
+school: str = "Holberton"  # variable school assigned str value "Holberton"
