e38cf37ea6559e6dbb81f70cd69b29b8ddc281af
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-03 17:35:58 -0400

    Module sets type-annotated function that takes a float argument and coverts it into a string.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/sur/bin/env python3
+"""
+Module creates type-annotated function that takes a float argument
+and returns string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """ Function returns string representation of float """
+    return str(n)
