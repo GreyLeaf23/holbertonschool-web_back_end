1bee184ab5825194b3848f20662ce56828ffa8c4
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-03 17:06:45 -0400

    Module sets type-annotated function that takes two string arguments and concantenates them.

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module creates type-annotated function that takes two string arguments
+and returns them concatenated.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Function returns concatenated string """
+    return str1 + str2
