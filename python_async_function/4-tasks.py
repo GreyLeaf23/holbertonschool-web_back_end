446383bdbfbb1258f9f76642db0fc5f07a2f6e6f
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-04 12:24:23 -0400

    Module defines a function that measure the time delay of time_wait_random function.

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,10 +4,11 @@ Module defines a function that' similar to wait_n but with the coroutine of
 task_wait_random.
 """
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
     Spawns wait_random n times with the specified max_delay.
     """
