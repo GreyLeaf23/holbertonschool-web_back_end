ed1e79db8c1250e7f3d629bf416546f6d33c421a
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-05 10:03:09 -0400

    Module defines coroutine that measures the execute time of another coroutine.

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Module defines a measure_runtime coroutine that will execute async_comprehension
-four times in parallel using asyncio.gather.
+Module defines a measure_runtime coroutine that will execute
+async_comprehension four times in parallel using asyncio.gather.
 """
 import asyncio
 import time
