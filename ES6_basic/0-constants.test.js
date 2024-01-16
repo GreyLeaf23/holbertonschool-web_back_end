e1f6a2555af92c7bd580b47409f0ac5dbc2ca958
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-16 17:12:15 -0400

    Configurations.

--- ES6_basic/0-constants.test.js
+++ ES6_basic/0-constants.test.js
@@ -3,21 +3,18 @@ import { taskFirst, getLast, taskNext } from './0-constants';
 describe('eS6_basic/0-constants.js', () => {
   describe('taskFirst()', () => {
     it("should return 'I prefer const when I can.'", () => {
-      expect.assertions(1);
       expect(taskFirst()).toBe('I prefer const when I can.');
     });
   });
 
   describe('getLast()', () => {
     it("should return ' is okay'", () => {
-      expect.assertions(1);
       expect(getLast()).toBe(' is okay');
     });
   });
 
   describe('taskNext()', () => {
     it("should return 'But sometimes let is okay'", () => {
-      expect.assertions(1);
       expect(taskNext()).toBe('But sometimes let is okay');
     });
   });
