d3780656bb1ba1e6422483ceaa5d050167de6e76
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-16 16:04:56 -0400

    ESlint fix?

--- ES6_basic/0-constants.test.js
+++ ES6_basic/0-constants.test.js
@@ -3,19 +3,22 @@ import { taskFirst, getLast, taskNext } from './0-constants';
 describe('eS6_basic/0-constants.js', () => {
   describe('taskFirst()', () => {
     it("should return 'I prefer const when I can.'", () => {
+      expect.hasAssertions();
       expect(taskFirst()).toBe('I prefer const when I can.');
     });
   });
 
   describe('getLast()', () => {
     it("should return ' is okay'", () => {
-      expect(getLast()).toBe(" is okay");
+      expect.hasAssertions();
+      expect(getLast()).toBe(' is okay');
     });
   });
 
   describe('taskNext()', () => {
     it("should return 'But sometimes let is okay'", () => {
-      expect(taskNext()).toBe("But sometimes let is okay");
+      expect.hasAssertions();
+      expect(taskNext()).toBe('But sometimes let is okay');
     });
   });
 });
