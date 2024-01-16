9c70fc8a54b8d6333330141049bd87068edf3bc5
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-15 20:05:42 -0400

    Modify JS Script so that variables already declared aren't overwritten inside the conditional block.

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* jshint esversion: 6 */
+export default function taskBlock(trueOrFalse) {
+    let task = false;
+    let task2 = true;
+
+    if (trueOrFalse) {
+       task = true;
+       task2 = false;
+    }
+
+    return [task, task2];
+  }
