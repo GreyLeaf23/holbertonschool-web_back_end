ca39190fa7867df462697c10b19a762be43bec05
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-17 18:17:50 -0400

    Rewrote function to use ES6's 'for...of' operator and reassign variable typing.

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-  for (let i = 0; i < array.length; i++) {
-    const value = array[i];
-    array[i] = appendString + value;
+  for (const idx of array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
   }
 
   return array;
