b8eafbc4c46373966936eae9b2672f2587ad1b1b
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-18 10:26:28 -0400

    Implement 'IntializeRooms' function that returns 3 arrays of 'ClassRoom' objects with their respective sizes.

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
+export default function initializeRooms() {
   let room1 = new ClassRoom(19);
   let room2 = new ClassRoom(20);
   let room3 = new ClassRoom(34);
