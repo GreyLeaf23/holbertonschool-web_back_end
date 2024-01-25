3bbf0d4fd35765cdc49c45820f4680f08efa6ae9
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-25 12:30:15 -0400

    Implement function that returns a Promise.

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate API call
-    setTimeout(() => {
-      const apiResponse = 'API response';
-      resolve(apiResponse);
-    }, 2000);
-  });
-}
+    return new Promise(() => {});
+  }
