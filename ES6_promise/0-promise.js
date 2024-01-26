6655f20dc803d2f013c848fb3b6070e2f4393e43
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-26 09:27:34 -0400

    Modify files.

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,10 @@
-export default function getResponseFromAPI() {
+function getResponseFromAPI() {
   // eslint-disable-next-line
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('true');
+      reject(new Error('The fake API is not working currently'));
     });
   });
 }
+export default  getResponseFromAPI;
