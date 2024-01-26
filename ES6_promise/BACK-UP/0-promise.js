2ca9c9381c5bf3a105929c64e438049cb0257cb1
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-26 10:06:00 -0400

    Been coping

--- ES6_promise/BACK-UP/0-promise.js
+++ ES6_promise/BACK-UP/0-promise.js
@@ -1,10 +1,8 @@
-#!/usr/bin/node
 function getResponseFromAPI() {
-  // eslint-disable-next-line
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('true');
-      reject(new Error('Error'));
+      reject(new Error('API'));
     });
   });
 }
