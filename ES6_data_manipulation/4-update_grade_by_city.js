7a1b28d77621b863c69f380dbc54c307d0d94110
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-29 18:25:23 -0400

    Update code structure.

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -9,8 +9,13 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 
   const mapping = sortFilter.map((student) => {
       const newGrade = newGrades.filter((grade) => grade.studentId === student.id);
-      const grade = newGrade.length === 0 ? { grade: 'N/A' } : newGrade[0];
-      return { ...student, ...grade };
+      const updated_grade = { ...student };
+        if (newGrade[0]) {
+            updated_grade.grade = newGrade[0].grade;
+        } else {
+            updated_grade.grade = 'N/A';
+        }
+        return updated_grade;
     });
 
     return mapping;
